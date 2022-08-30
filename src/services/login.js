3ebb368695329d3515e4af0287a6fa772d37b31e
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-30 11:51:30 -0300

    adiciona função que busca dados no banco mysql

--- src/services/login.js
+++ src/services/login.js
@@ -16,13 +16,13 @@ const userLogin = {
       where: { email: mail, password },
     });
 
-    if (result) {
+    if (!result) return { code: 400, data: { message: 'Invalid fields' } };
+
     const { id, displayName, email, image } = result;
 
     const token = tokenHelper.createToken({ id, displayName, email, image });
 
-    return token;
-    }
+    return { code: 200, data: { token } };
   },
 };
 
--- src/services/users.js
+++ src/services/users.js
@@ -1,11 +1,23 @@
-const { Users } = require('../database/models');
+const { User } = require('../database/models');
+const tokenHelper = require('../../helpers/tokenHelper');
 
 const usersService = {
-        getUsers: async () => {
-        const result = await Users.findAll();
-        console.log(result);
-        return result;
-      },
-    };
+  getUsers: async () => {
+    const result = await User.findAll();
+    console.log(result);
+    return result;
+  },
+  getUsersByEmail: async (email) => {
+    const result = await User.findOne({
+      where: { email },
+    });
+    return result;
+  },
+  postUsers: async (user) => {
+    const { id, displayName, email, image } = await User.create(user);
+    const token = tokenHelper.createToken({ id, displayName, email, image });
+    return { code: 201, data: { token } };
+  },
+};
 
 module.exports = usersService;
