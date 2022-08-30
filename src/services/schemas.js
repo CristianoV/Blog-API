54bc589838f96fb2de3af21bf59b3c5059e260be
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-30 11:39:00 -0300

    feat: adiciona validações para criação de usuarios e de login

--- src/services/schemas.js
+++ src/services/schemas.js
@@ -1,13 +1,26 @@
 const joi = require('joi');
 
 const login = joi.object({
-    email: joi.string().required().messages({
-        'any.required': '400|Some required fields are missing',
-    }),
-    password: joi.string().min(5).required().messages({
-      'string.min': '422|"name" length must be at least 5 characters long',
-      'any.required': '400|Some required fields are missing',
-    }),
-  });
+  email: joi.string().min(1).required().messages({
+    'string.empty': '400|Some required fields are missing',
+    'any.required': '400|Some required fields are missing',
+  }),
+  password: joi.string().required().messages({
+    'string.empty': '400|Some required fields are missing',
+    'any.required': '400|Some required fields are missing',
+  }),
+});
 
-module.exports = { login };
+const newUser = joi.object({
+  email: joi.string().email().min(1).messages({
+    'string.email': '400|"email" must be a valid email',
+  }),
+  password: joi.string().min(6).required().messages({
+    'string.min': '400|"password" length must be at least 6 characters long',
+  }),
+  displayName: joi.string().min(8).required().messages({
+    'string.min': '400|"displayName" length must be at least 8 characters long',
+  }),
+});
+
+module.exports = { login, newUser };
