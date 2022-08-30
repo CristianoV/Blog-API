96eda032936af916144db2a93b7291583f27e4c3
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-30 11:50:41 -0300

    feat: adiciona função que retorna com codigo e json provida pelo json

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,10 +1,25 @@
+const userLogin = require('../services/login');
+const { newUser } = require('../services/schemas');
 const usersService = require('../services/users');
 
 const usersController = {
-    getUsers: async (req, res) => {
-        const result = await usersService.getUsers();
-        res.code(200).json(result);
-    },
+  getUsers: async (req, res) => {
+    const result = await usersService.getUsers();
+    res.code(200).json(result);
+  },
+  postUsers: async (req, res) => {
+    const { body } = req;
+    const { email, password, displayName } = body;
+
+    await userLogin.LoginError(newUser, { email, password, displayName });
+
+    const emailValidate = await usersService.getUsersByEmail(body.email);
+
+    if (emailValidate) return res.status(409).json({ message: 'User already registered' });
+
+    const { code, data } = await usersService.postUsers(body);
+    res.status(code).json(data);
+  },
 };
 
 module.exports = usersController;
