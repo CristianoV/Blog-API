8f5b4a7df6acfbbf3b5c57d21795b1cf595778fa
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-30 11:44:07 -0300

    fix: reformulado model para uma melhor perfomance

--- src/database/models/postcategories.js
+++ src/database/models/postcategories.js
@@ -1,23 +1,20 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class PostCategories extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  PostCategories.init({
-    postId: DataTypes.INTEGER
+  const PostCategories = sequelize.define("PostCategories", {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
   }, {
-    sequelize,
     modelName: 'PostCategories',
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false,
   });
+
+  // Character.associate = (models) => {
+  //   Character.belongsTo(models.Job, {
+  //     foreignKey: "jobId",
+  //     as: "job"
+  //   });
+  // };
+
   return PostCategories;
 };
