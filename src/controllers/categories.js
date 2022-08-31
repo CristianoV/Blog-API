4603aafb6c273a695fc8e69ad77cef9032a49047
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-31 00:20:32 -0300

    fixed: adiciona função que valida dados para pesquisa no banco de dados

---
+++ src/controllers/categories.js
@@ -0,0 +1,15 @@
+const categoriesServices = require('../services/categories');
+const usersService = require('../services/users');
+
+const categoriesController = {
+    newCategory: async (req, res) => {
+        const { name } = req.body;
+        if (!name) { return res.status(400).json({ message: '"name" is required' }); }
+        await usersService.validToken(req);
+        const { code, data } = await categoriesServices.newCategory(name);
+
+        res.status(code).json(data);
+    },
+};
+
+module.exports = categoriesController;
