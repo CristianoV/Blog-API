a6095e1594c3c7b39e89bb4ecc7d7c58bba95d86
Cristiano Vieira <cristianoviieira@gmail.com>
2022-09-02 14:33:32 -0300

    adiciona novas funções que se comunicam diretamente com o banco de dados sobre categorias

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,17 +1,33 @@
 const { Category } = require('../database/models');
 // const { validator } = require('./validator');
-// const NotFoundError = require('../errors/NotFoundError');
+const NotFoundError = require('../errors/NotFoundError');
 // const tokenHelper = require('../../helpers/tokenHelper');
 
 const categoriesService = {
-    newCategory: async (name) => {
-        await Category.create({ name });
-        return { code: 201, data: { name } };
-    },
-    getCategories: async () => {
-        const categories = await Category.findAll();
-        return { code: 200, data: categories };
-    },
+  allCategories: async () => {
+    const categories = await Category.findAll();
+    return categories;
+  },
+  validCategory: async (categoryIds) => {
+    const allCategories = await categoriesService.allCategories();
+
+    const newPostCategorys = allCategories.map(({ id }) => id);
+
+    const check = categoryIds.every((id) => newPostCategorys.includes(id));
+
+    if (!check) {
+      console.log('xablau');
+      throw new NotFoundError('"categoryIds" not found', 400);
+    }
+  },
+  newCategory: async (name) => {
+    await Category.create({ name });
+    return { code: 201, data: { name } };
+  },
+  getCategories: async () => {
+    const categories = await Category.findAll();
+    return { code: 200, data: categories };
+  },
 };
 
 module.exports = categoriesService;
