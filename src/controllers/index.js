9321f72151f6d0fbf72e1636929ca77294aae03f
Cristiano Vieira <cristianoviieira@gmail.com>
2022-09-02 14:22:14 -0300

    fix: codigo refatorado para uma melhor facilitação na implementação de novos features

---
+++ src/controllers/index.js
@@ -0,0 +1,6 @@
+const categories = require('./categories');
+const login = require('./login');
+const post = require('./post');
+const user = require('./users');
+
+module.exports = { categories, login, post, user };
