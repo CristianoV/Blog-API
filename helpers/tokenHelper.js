372721d27058f17d88cfb6a716edc824bd2ac885
Cristiano Vieira <cristianoviieira@gmail.com>
2022-09-02 14:26:12 -0300

    fix: novo tempo maximo de usuario logado

--- helpers/tokenHelper.js
+++ helpers/tokenHelper.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 
 // const JWT_SECRET = 'batatinha';
-const JWT_CONFIG = { algorithm: 'HS256', expiresIn: '15min' };
+const JWT_CONFIG = { algorithm: 'HS256', expiresIn: '120min' };
 
 const createToken = (payload) => {
   const token = jwt.sign(payload, JWT_SECRET, JWT_CONFIG);
