ff7fd4a33653f041fab5524a4bf27d9674962bdb
Cristiano Vieira <cristianoviieira@gmail.com>
2022-09-05 09:38:44 -0300

    feat: adicionado função que deleta posts do banco de dados

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -1,27 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
-  const PostCategories = sequelize.define("PostCategory", {
-    postId: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER,
-  }, {
-    modelName: 'PostCategories',
-    timestamps: false,
-    createdAt: false,
-    updatedAt: false,
-  });
+  const PostCategories = sequelize.define(
+    'PostCategory',
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      categoryId: DataTypes.INTEGER,
+    },
+    {
+      modelName: 'PostCategories',
+      timestamps: false,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
 
   PostCategories.associate = (models) => {
     models.BlogPost.belongsToMany(models.Category, {
-      as: "categories",
+      as: 'categories',
       through: PostCategories,
-      foreignKey: "postId",
-      otherKey: "categoryId",
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
     });
-    
+
     models.Category.belongsToMany(models.BlogPost, {
-      as: "BlogPosts",
+      as: 'BlogPosts',
       through: PostCategories,
-      foreignKey: "categoryId",
-      otherKey: "postId",
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
     });
   };
 
